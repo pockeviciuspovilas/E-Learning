b36f3921ad678e661ad58325d09476752cc8804b
pockeviciuspovilas <pockeviciuspovilas@gmail.com>
2020-04-16 01:29:10 +0300

    Initial commit

---
+++ Elearn/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Elearn.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Principal;
+
+namespace Elearn.Controllers
+{
+    public class HomeController : Controller
+    {
+        aspnetElearnContext context = new aspnetElearnContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [Authorize(Roles = "SuperAdmin")]
+        public IActionResult Users()
+        {
+            List<AspNetUsers> users = context.AspNetUsers.ToList();
+            return View(users);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
